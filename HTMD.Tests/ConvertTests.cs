5e3a27a3b77fee903771d96edcd917a67277b677
JonathanBout <jonathanbout01@gmail.com>
2022-07-14 14:24:19 +0200

    Add project files.

---
+++ HTMD.Tests/ConvertTests.cs
@@ -0,0 +1,61 @@
+using DeltaDev.HTMD;
+using System;
+
+namespace DeltaDev.HTMD.Tests
+{
+    [TestClass]
+    public class SingleStatementTests
+    {
+        [TestMethod]
+        public void BlockQuote()
+        {
+            string htmd = ">Hello, World!";
+            string expectedHtml = "<blockquote>Hello, World!</blockquote>";
+
+            Assert.AreEqual(expectedHtml, HTMDConvert.SingleStatementToHTML(htmd)); 
+        }
+
+        [TestMethod]
+        public void Italic()
+        {
+            string htmd = "*Hello, World!*";
+            string expectedHtml = "<i>Hello, World!</i>";
+
+            Assert.AreEqual(expectedHtml, HTMDConvert.SingleStatementToHTML(htmd));
+        }
+
+        [TestMethod]
+        public void Bold()
+        {
+            string htmd = "**Hello, World!**";
+            string expectedHtml = "<b>Hello, World!</b>";
+
+            Assert.AreEqual(expectedHtml, HTMDConvert.SingleStatementToHTML(htmd));
+        }
+
+        [TestMethod]
+        public void OrderedList()
+        {
+            string htmd = "1. Item 1";
+            string expectedHtml = "<li>Item 1</li>";
+            Assert.AreEqual(expectedHtml, HTMDConvert.SingleStatementToHTML(htmd));
+        }
+
+        [TestMethod]
+        public void UnorderedList()
+        {
+            string htmd = "- Item 1";
+            string expectedHtml = "<li>Item 1</li>";
+            Assert.AreEqual(expectedHtml, HTMDConvert.SingleStatementToHTML(htmd));
+        }
+
+        [TestMethod]
+        public void Headings()
+        {
+            string htmd = "# HEADING 1";
+                                  
+            string expectedHtml = @"<h1>HEADING 1</h1>";
+            Assert.AreEqual(expectedHtml, HTMDConvert.SingleStatementToHTML(htmd));
+        }
+    }
+}
---
+++ HTMD/HTMDConvert.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DeltaDev.HTMD
+{
+    public static class HTMDConvert
+    {
+        //public static readonly string[] mdRegEx = new[] { "#{1,3}", "*", ">", "[1-9]{1,2}." };
+        public static readonly MDRegEx[] mdRegEx = new MDRegEx[]
+        {
+            new MDRegEx(new Regex(@"(?<=#{3})(.*)"),
+                "<h3>", "</h3>"),                                                   //heading 3         |
+            new MDRegEx(new Regex(@"(?<=#{2})(.*)"),
+                "<h2>", "</h2>"),                                                   //heading 2         |
+            new MDRegEx(new Regex(@"(?<=#{1})(.*)"),
+                "<h1>", "</h1>"),                                                   //heading 1         |
+            new MDRegEx(new Regex(@"(?<=\*{2}).*?(?=\*{2})"),
+                "<b>", "</b>"),                                                     //bold              |
+            new MDRegEx(new Regex(@"(?<=\*{1}).*?(?=\*{1})"),
+                "<i>", "</i>"),                                                     //italic            |
+            new MDRegEx(new Regex(@"(?<=>)(.*)"),
+                "<blockquote>", "</blockquote>"),                                   //blockquote        |
+            new MDRegEx(new Regex(@"(?<=[1-9]{1,2}\.(\s?))(.*)"),
+                "<li>", "</li>", "<ol>", "</ol>"),                                  //ordered list      |
+            new MDRegEx(new Regex(@"(?<=\-)(.*)"),
+                "<li>", "</li>", "<ul>", "</ul>")                                   //unordered list    |
+        };
+
+        
+        public static string ToHTML(string htmd)
+        {
+            return "";
+        }
+
+        public static string SingleStatementToHTML(string statement)
+        {
+            MDRegEx match = mdRegEx.FirstOrDefault(x => x.pattern is not null && x.pattern.IsMatch(statement), default);
+            if (match.pattern is not null)
+            {
+                var startMatch = match.pattern.Match(statement);
+                return match.htmlOpenTag + startMatch.Groups[0].Value.Trim() + match.htmlCloseTag;
+            }
+            return "";
+        }
+
+        public static string Between(this string input, string start, string end)
+        {
+            string FinalString;
+            int Pos1 = input.IndexOf(start) + start.Length;
+            int Pos2 = input.IndexOf(end);
+            FinalString = input[Pos1..Pos2];
+            return FinalString;
+        }
+
+        public readonly struct MDRegEx
+        {
+            public readonly Regex? pattern = null;
+            public readonly string? htmlOpenTag = null;
+            public readonly string? htmlCloseTag = null;
+            public readonly string? parentOpenTag = null;
+            public readonly string? parentCloseTag = null;
+
+            public MDRegEx(Regex pattern, string htmlOpen, string htmlClose)
+            {
+                this.pattern = pattern;
+                htmlOpenTag = htmlOpen;
+                htmlCloseTag = htmlClose;
+            }
+
+            public MDRegEx(Regex pattern, string htmlOpen, string htmlClose, string parentOpen, string parentClose)
+            {
+                this.pattern = pattern;
+                htmlOpenTag = htmlOpen;
+                htmlCloseTag = htmlClose;
+                parentOpenTag = parentOpen;
+                parentCloseTag = parentClose;
+            }
+
+            public static bool operator ==(MDRegEx left, MDRegEx right)
+            {
+                return left.pattern == right.pattern;
+            }
+
+            public static bool operator !=(MDRegEx left, MDRegEx right)
+            {
+                return left.pattern != right.pattern;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                var other = (MDRegEx?)obj;
+                return other == this;
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+    }
+}
